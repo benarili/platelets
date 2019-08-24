d02113243538bc2acc55ded7b3947a31b2cb583f
yishaia zabary <yz@sky-pick.com>
2019-08-24 15:09:33 +0300

    changing some pixels valued [180 180 180] to some bluish color

--- InputReader.py
+++ InputReader.py
@@ -12,14 +12,27 @@ buf = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
 
 fc = 0
 ret = True
-
+gray_array = np.ndarray(shape=(3,), dtype=int, order='F', buffer=np.array([180, 180, 180]))
+red_array = np.ndarray(shape=(3,), dtype=int, order='F', buffer=np.array([246, 7, 31]))
 while (fc < frameCount  and ret):
     ret, buf[fc] = cap.read()
+    col_ctr = 0
+    for col in buf[fc]:
+        rgb_ctr = 0
+        for rgb in col:
+            if (rgb == gray_array).all():
+                buf[fc][col_ctr][rgb_ctr] = red_array
+            rgb_ctr += 1
+        col_ctr += 1
     fc += 1
-
 cap.release()
 
-print(buf[9].header)
+if (gray_array == buf[9][0][0]).all():
+    print('yes')
+else:
+    print('no')
+# print(np.ndarray(shape=(3,), dtype=int, order='F', buffer=np.array([180,180,180])))
+print(buf[9][0][0])
 
 cv2.namedWindow('frame 10')
 cv2.imshow('frame 10', buf[9])
